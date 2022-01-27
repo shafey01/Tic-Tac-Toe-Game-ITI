15a265150127f6f1dc16e4e59399d004836c741b
Mustafa-Hammad <96911067+Mustafa-Hammad@users.noreply.github.com>
2022-01-27 20:19:06 +0200

    Mustafa Raed GUI

---
+++ Login/src/login/FXMLDocumentController.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML2.java to edit this template
+ */
+package login;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author Mustafa Raed
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    @FXML
+ 
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
