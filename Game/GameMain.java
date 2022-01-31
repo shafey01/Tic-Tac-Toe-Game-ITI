1c41728a6284264904e5cde77fba9369eb5a7486
Michael Ramez <34134110+michaelramez@users.noreply.github.com>
2022-01-31 21:13:34 +0200

    game console

--- Game/GameMain.java
+++ Game/GameMain.java
@@ -2,7 +2,7 @@ package Game;
 
 public class GameMain {
     public static void main(String[] args) {
-        NewGame game = new NewGame();
-        game.displayBoard();
+        new GameWithComputer();
+
     }
 }
